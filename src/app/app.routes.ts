06470fb95c82f30a7e6e6982c7f1c6037ed55b21
David Guevara <dasanguepa@gmail.com>
2026-09-23 11:29:01 -0500

    feat: setup application config and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,58 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { MaterialListComponent } from './materials/material-list/material-list.component';
+import { MaterialFormComponent } from './materials/material-form/material-form.component';
+import { ProjectListComponent } from './projects/project-list/project-list.component';
+import { ProjectFormComponent } from './projects/project-form/project-form.component';
+import { ProjectMaterialsComponent } from './projects/project-materials/project-materials.component';
+import { ProjectReportComponent } from './projects/project-report/project-report.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'materials',
+        pathMatch: 'full',
+      },
+      {
+        path: 'materials',
+        component: MaterialListComponent,
+      },
+      {
+        path: 'materials/new',
+        component: MaterialFormComponent,
+      },
+      {
+        path: 'materials/edit/:id',
+        component: MaterialFormComponent,
+      },
+      {
+        path: 'projects',
+        component: ProjectListComponent,
+      },
+      {
+        path: 'projects/new',
+        component: ProjectFormComponent,
+      },
+      {
+        path: 'projects/edit/:id',
+        component: ProjectFormComponent,
+      },
+      {
+        path: 'projects/:id/materials',
+        component: ProjectMaterialsComponent,
+      },
+      {
+        path: 'projects/:id/report',
+        component: ProjectReportComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'materials',
+  },
+];
